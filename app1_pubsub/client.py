5f33335d2079f80d508b3cd7e866212a5657752d
Victor Feitosa Lourenco <victor.240398@hotmail.com>
2021-07-12 00:14:53 -0300

    Added first cryptography attempt to messaging and updated UI

--- app1_pubsub/client.py
+++ app1_pubsub/client.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
-Created on July 10 2021
+""" ===========================================================================
+Created on July 8 2021
 
 @authors: Lucas Ricardo Marques de Souza
           Victor Feitosa Lourenco
 
 This python script was created for the Distribuited Systems course for the
 Computer Engineering major at the Federal University of Technology - Parana.
-"""
+=========================================================================== """
 
 """ --------- IMPORTS --------- """
 # Libraries
 import os
 import sys
 import time
-import Pyro4
-import Pyro4.util
-from rich import console
 import stdiomask
 from rich import print
 from rich.console import Console
 from prettytable import PrettyTable
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+import Pyro4
+import Pyro4.util
 
 # Internal Modules
 from server import Server
@@ -43,88 +45,171 @@ class Client(object):
     - password (str): client password.
     - priv_key -TODO- FINISH THIS
     - pub_key 
-    - object  
+    - reference  
         
     Methods:
-    - d: ;
-    - a: .
+    - registerInServer: ;
+    - displaySubscriptions: .
     """
 
     def __init__(self, name, contact, password):
         self.name = name
         self.contact = contact
         self.password = password
+
         # Private and public keys
-        self.priv_key = 1
-        self.pub_key = 2
-        self.object = 1
+        self.priv_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        self.pub_key = self.priv_key.public_key()
+
+        # Remote object reference -TODO- WITH PYRO
+        self.reference = 1
+
 
     def registerInServer(self, server):
-        print(f"This is {self.name} registering in server.")        
-        server.addClient(self.name,self.contact,self.pub_key)
+        """
+        Description: calls server method to register new client; this is done
+                     on creation of the object.
+        
+        Parameters:
+        - server (Server): Server instance which is hosting the application.
 
-    def displaySubscriptions(self, processes):
+        Returns:
+        - None
+        """
+        print(f"This is {self.name} registering in server.")
+        # -TODO- CHECK IF CLIENT ISN'T ALREADY REGISTERED
+        server.addClient(self.name, self.contact, self.pub_key)
+
+
+    @staticmethod
+    def displaySubscriptions(subs):
+        """
+        Description: auxiliary function which organizedly displays a client's
+                     subscriptions (rides and requests).
+        
+        Parameters:
+        - subs (tuple): contains two lists of subscriptions, the first
+                        with the client's requests and the seconds with
+                        their rides.
+
+        Returns:
+        - None
+        """
         requests_table = PrettyTable()
         rides_table = PrettyTable()
 
         print(" - Want to be a passenger:")
-        if processes[0] == []:
+        if subs[0] == []:
             print("\tNot subscribed to any rides yet!")
         else:
             requests_table.field_names = ["ID", "Origin", "Destination", "Date"]
-            for request in processes[0]:
-                requests_table.add_row([request.id, request.origin,
-                                        request.destination, request.date])
+            for request in subs[0]:
+                requests_table.add_row([request["id"], request["origin"],
+                                        request["destination"], request["date"]])
+            sys.stdout.write(str(requests_table))
+            print()
+            # print(requests_table)
         
         print("\n - Want to be a driver:")
-        if processes[1] == []:
+        if subs[1] == []:
             print("\tNot subscribed to any rides yet!")
         else:
             rides_table.field_names = ["ID", "Origin", "Destination", "Date", "Passengers"]
-            for ride in processes[1]:
-                rides_table.add_row([ride.id, ride.origin, ride.destination,
-                                     ride.date, ride.passengers])
+            for ride in subs[1]:
+                rides_table.add_row([ride["id"], ride["origin"], ride["destination"],
+                                     ride["date"], ride["passengers"]])
+            sys.stdout.write(str(requests_table))
+            print()
+            # print(rides_table)
+
 
     def delSubscription(self, server):
         pass
 
+
     def addSubscription(self, server):
-        print("Do you want to be a driver or a passenger?")
-        value = int(input(" 1 - Driver \n 2 - Passenger \n 3 - Return to user menu"))
+        """
+        Description: call host server to add a subscription to this Client. This
+                     method also communicates with the user regarding the details
+                     of the subscription (which can be a ride or a request).
+        
+        Parameters:
+        - server (Server): Server instance which is hosting the application.
+
+        Returns:
+        - None
+        """
+        print(" ------ [bold] Adding Subscription [/bold] ------")
+        print("Do you want to be a driver or a passenger?\n"
+              " 1 - Driver \n 2 - Passenger \n 3 - Return to user menu")
+        value = int(input())
         while(value not in [1, 2, 3]):
             print("[bold underline]Invalid option![/bold underline]")
             value = int(input())
+        os.system('cls')
+        print(" ------ [bold] Adding Subscription [/bold] ------")
         
-        if value == 1:
-            print("OK! We need to know a few things about the ride you want to give.")
-            origin = input(" Origin location: ")
-            dest = input(" Destination location: ")
-            date = input(" Date of the ride: ")
-            n_pass = int(input(" Max number of passengers: "))
+        message = {"name":self.name, "reference":self.reference}
 
-            server.addRide(self.object, self.name, origin, dest, date, n_pass)
-            print("New subscription successfully added! Returning to user menu...")
-            time.sleep(2)
+        if value == 1:
+            print("OK! We need to know a few things about the ride you want to give.\n"
+                  " [bold bright_cyan]Origin location[/bold bright_cyan]: ", end="")
+            message["origin"] = str(input())
+            print(" [bold bright_cyan]Destination location[/bold bright_cyan]: ", end="")
+            message["destination"] = str(input())
+            print(" [bold bright_cyan]Date of the ride[/bold bright_cyan]: ", end="")
+            message["date"] = str(input())
+            print(" [bold bright_cyan]Max number of passengers[/bold bright_cyan]: ", end="")
+            message["passengers"] = str(input())
         
         elif value == 2:
-            print("OK! We need to know a few things about the ride you want to take.")
-            origin = input(" Origin location: ")
-            dest = input(" Destination location: ")
-            date = input(" Date of the ride: ")
+            print("OK! We need to know a few things about the ride you want to take.\n"
+                  " [bold bright_cyan]Origin location[/bold bright_cyan]: ", end="")
+            message["origin"] = str(input())
+            print(" [bold bright_cyan]Destination location[/bold bright_cyan]: ", end="")
+            message["destination"] = str(input())
+            print(" [bold bright_cyan]Date of the ride[/bold bright_cyan]: ", end="")
+            message["date"] = str(input())
 
-            server.addRideRequest(self.object, self.name, origin, dest, date)
-            print("New subscription successfully added! Returning to user menu...")
-            time.sleep(2)
+        # Turns dictionary message to string and encodes it to bytes
+        message = str(message).encode('utf_8')
+        signature = self.priv_key.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
 
+        server.addSubscription(message, signature)
+        print("New subscription successfully added! Returning to user menu...")
+        time.sleep(3)
+        
 
     def interact(self, server):
+        """
+        Description: represents the client menu, where the user will interact
+                     with the host server manage their subscriptions.
+        
+        Parameters:
+        - server (Server): Server instance which is hosting the application.
+
+        Returns:
+        - None
+        """
         while True:
+            print(" ------ [bold] User Menu [/bold] ------")
             console = Console()
-            print("\tHello ", end="")
+            print("Hello ", end="")
             console.print(self.name, style="bold orange3", end="")
-            print("! These are your active subscriptions:")
+            print("!\nThese are your active subscriptions:\n")
             
-            subs = server.getClientSubscriptions(self.object)
+            subs = server.getClientSubscriptions(self.reference)
             self.displaySubscriptions(subs)
 
             print("\nWhat do you wish to do?\n 1 - Delete subscription \n 2 - Add subscription \n 3 - Logout")
@@ -144,11 +229,51 @@ class Client(object):
                 os.system('cls')
                 return 0
 
+
     def notifyAvailableRide(self, name, contact):
-        print(f"NOTIFICATION: Available Ride from {name} and contact: {contact}")
+        """
+        Description: method called by the server whenever a request subscription
+                     from the client is met at the host, thus notifying the user.
+        
+        Parameters:
+        - name (string): name of the OTHER client/user that meets one of THIS
+                         client's request subscriptions;
+        - contact (string): contact of the OTHER client/user that meets one
+                            of THIS client's request subscriptions.
+
+        Returns:
+        - None
+        """
+        console = Console()
+        print("\n[bold]NOTIFICATION ON [/bold]", end="")
+        console.print(self.name, style="bold orange3", end="")
+        print(": Available ride from ")
+        console.print(name, style="bold magenta", end="")
+        print(" and contact: ")
+        console.print(contact, style="bold magenta", end="")
+
 
     def notifyAvailablePassenger(self, name, contact):
-        print(f"NOTIFICATION: Available Passenger: {name}, contact: {contact}")
+        """
+        Description: method called by the server whenever a ride subscription
+                     from the client is met at the host, thus notifying the user.
+        
+        Parameters:
+        - name (string): name of the OTHER client/user that meets one of THIS
+                         client's ride subscriptions;
+        - contact (string): contact of the OTHER client/user that meets one
+                            of THIS client's ride subscriptions.
+
+        Returns:
+        - None
+        """
+        console = Console()
+        print("\n[bold]NOTIFICATION ON [/bold]", end="")
+        console.print(self.name, style="bold orange3", end="")
+        print(": Available passenger ")
+        console.print(name, style="bold magenta", end="")
+        print(", contact: ")
+        console.print(contact, style="bold magenta", end="")
 
 
 class Interface():
@@ -168,8 +293,14 @@ class Interface():
     - current_client (Client): currently active client.
         
     Methods:
-    - details: returns a string containing the information of the dataset;
-    - append_exception: append original column name to exceptions;
+    - setState: update method for the self.state attribute;
+    - getClient: consults the list of clients for a given client;
+    - mainMenu: main menu method with user interaction through the terminal;
+    - loginMenu: login menu method with user interaction through the terminal;
+    - registerMenu: client register menu method with user interaction through
+                    the terminal;
+    - clientMenu: client menu method which interacts with the self.current_client
+                  attribute.
     """
 
     def __init__(self):
@@ -199,11 +330,12 @@ class Interface():
 
     def mainMenu(self):
         """
-        Description: main menu function, not yet related to a specific Client. Introduces
-                     the application and asks the user to choose the next action. This
-                     structure (question and answer) will be used for the following menus.
-                     Additionally, the self.state attribute will be updated accordingly,
-                     serving as the return of the function.
+        Description: main menu function, not yet related to a specific Client.
+                     Introduces the application and asks the user to choose the
+                     next action. This structure (question and answer) will be
+                     used for the following menus. Additionally, the self.state
+                     attribute will be updated accordingly, serving as the
+                     return of the function.
         
         Parameters:
         - None
@@ -212,8 +344,8 @@ class Interface():
         - None
         """
         print(" ---- [bold]Welcome to [red]RideTogether![/red][/bold] ----")
-        print("Are you already a user? Login by typing 1!")
-        print("If not, type 2 to register now!\n 1 - Login \n 2 - Register\n 9 - Exit")
+        print("Are you already a user? Want to login or register?")
+        print("Text in this [bold bright_cyan]color[/bold bright_cyan] represent options to be selected!\n 1 - Login \n 2 - Register\n 9 - Exit")
         self.state = int(input())
         os.system('cls')
     
@@ -228,11 +360,14 @@ class Interface():
         Returns:
         - None
         """
-        print("Log into your account with your email and password!")
-        email = input("Email: ")
-        password = stdiomask.getpass()
-        client = self.getClient(email, password)
+        print(" ------ [bold] Login [/bold] ------")
+        print("Log into your account with your email and password.\n"
+              " [bold bright_cyan]Email[/bold bright_cyan]: ", end="")
+        email = input()
+        print(" [bold bright_cyan]Password[/bold bright_cyan]: ", end="")
+        password = stdiomask.getpass(prompt="")
 
+        client = self.getClient(email, password)
         if client == None:
             print("User not found! Please try again...\n 1 - Login \n 0 - Main menu")
             self.state = int(input())
@@ -240,7 +375,7 @@ class Interface():
             console = Console()
             print("\nWelcome ", end="")
             console.print(client.name, style="bold orange3", end="")
-            print("!\nLogging in...\n\n")
+            print("!\nLogging in...")
             self.current_client = client
             self.state = 3
             time.sleep(2)
@@ -248,10 +383,10 @@ class Interface():
         os.system('cls')
 
 
-    def registerMenu(self):
+    def registerMenu(self, server):
         """
-        Description: client register menu function, prompts the user for name, email and
-                     password to register them in the application.
+        Description: client register menu function, prompts the user for name,
+                     email and password to register them in the application.
         
         Parameters:
         - None
@@ -259,19 +394,20 @@ class Interface():
         Returns:
         - None
         """
-        print("To become a user of [bold red]RideTogether![/bold red], first type in your name.")
-        name = input(" Name: ")
-        print("\n Next step, your email.")
-        email = input("Email: ")
-        print("\n Finally, choose a password for your account.")
-        password = stdiomask.getpass()
+        print(" ------ [bold] Register [/bold] ------")
+        print("To become a user of [bold red]RideTogether![/bold red], "
+              "first type in your name.\n [bold bright_cyan]Name[/bold bright_cyan]: ", end="")
+        name = input()
+        print("\nNext step, your email.\n [bold bright_cyan]Email[/bold bright_cyan]: ", end="")
+        email = input()
+        print("\nFinally, choose a password for your account.\n [bold bright_cyan]Password[/bold bright_cyan]: ", end="")
+        password = stdiomask.getpass(prompt="")
+        os.system('cls')
+        
+        self.clients.append(Client(name, email, password))
+        self.clients[-1].registerInServer(server)
 
-        self.clients.append(Client(name, email))
-
-        console = Console()
-        print("\nYou were registered successfully, ", end="")
-        console.print(name, style="bold orange3", end="")
-        print("!\n 0 - Return to main menu")
+        print("\n 0 - Return to main menu")
         self.state = int(input())
         
         os.system('cls')
@@ -279,7 +415,8 @@ class Interface():
 
     def clientMenu(self, server):
         """
-        Description: client menu function to simply call the correct client's method.
+        Description: client menu function to simply call the correct client's
+                     method.
         
         Parameters:
         - None
@@ -293,9 +430,9 @@ class Interface():
 
 def main():
     server = Server() 
-    # joao = Client("Joao","123")
-    # maria = Client("Maria","123123")
-    # athena = Client("Athena","bumbum")
+    # joao = Client("Joao","123", "a")
+    # maria = Client("Maria","123123", "b")
+    # athena = Client("Athena","bumbum", "c")
 
     interface = Interface()
     while(interface.state != 9):
@@ -304,13 +441,14 @@ def main():
         elif interface.state == 1:
             interface.loginMenu()
         elif interface.state == 2:
-            interface.registerMenu()
+            interface.registerMenu(server)
         elif interface.state == 3:
             interface.clientMenu(server)
         elif interface.state != 9:
             os.system('cls')
             interface.setState(0)
-            print("[bold underline]Invalid option![/bold underline] Please select one of the shown numbers.\n")
+            print("[bold underline]Invalid option![/bold underline] "
+                  "Please select one of the shown numbers.\n")
 
 
 if __name__=="__main__":
--- app1_pubsub/server.py
+++ app1_pubsub/server.py
@@ -1,5 +1,27 @@
-import Pyro4
+# -*- coding: utf-8 -*-
+""" ===========================================================================
+Created on July 8 2021
+
+@authors: Lucas Ricardo Marques de Souza
+          Victor Feitosa Lourenco
+
+This python script was created for the Distribuited Systems course for the
+Computer Engineering major at the Federal University of Technology - Parana.
+=========================================================================== """
+
+""" --------- IMPORTS --------- """
+# Libraries
 import random
+from ast import literal_eval
+from rich import print
+from rich import console
+from rich.console import Console
+import Pyro4
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.exceptions import InvalidSignature
+""" ------------------------ """
 
 # Start name server
 ## python -m Pyro4.naming
@@ -21,31 +43,53 @@ class Server(object):
             "contact" : contact,
             "publickey" : public_key
         })
+        console = Console()
+        print("\n[bold chartreuse3]Server[/bold chartreuse3]: ", end="")
+        console.print(name, style="bold orange3", end="")
+        print(" registered successfully.")
 
-    def addRideRequest(self, object, name, origin, destination, date):
+
+
+    def addSubscription(self, message, signature):
         self.current_id += 1
-        self.requests.append({
-            "id" : self.current_id,
-            "object" : object,
-            "name": name,
-            "origin" : origin,
-            "destination" : destination,
-            "date" : date
-        }) 
 
-        return self.current_id
+        decoded_message = literal_eval(message.decode("utf-8"))
+        client = next(client for client in self.clients if client["name"] == decoded_message["name"])
 
-    def addRide(self, object, name, origin, destination, date, n_passengers):
-        self.current_id += 1
-        self.rides.append({
-            "id" : self.current_id,
-            "object" : object,
-            "name": name,
-            "origin" : origin,
-            "destination" : destination,
-            "date" : date,
-            "passengers" : n_passengers
-        })  
+        try:
+            client["publickey"].verify(
+                signature,
+                message,
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            print("\n[bold chartreuse3]Server[/bold chartreuse3]: Valid signature at the server!")
+        except InvalidSignature:
+            print("\n[bold chartreuse3]Server[/bold chartreuse3]: Invalid signature")
+
+        # Identifies if the message relates to a request or ride
+        if len(decoded_message) == 5:
+            self.requests.append({
+                "id"          : self.current_id,
+                "reference"   : decoded_message["reference"],
+                "name"        : decoded_message["name"],
+                "origin"      : decoded_message["origin"],
+                "destination" : decoded_message["destination"],
+                "date"        : decoded_message["date"]
+            })
+        else:
+            self.rides.append({
+                "id"          : self.current_id,
+                "reference"   : decoded_message["reference"],
+                "name"        : decoded_message["name"],
+                "origin"      : decoded_message["origin"],
+                "destination" : decoded_message["destination"],
+                "date"        : decoded_message["date"],
+                "passengers"  : decoded_message["passengers"]
+            })
 
         return self.current_id
 
@@ -62,12 +106,12 @@ class Server(object):
                                             and ride["destination"] == destination 
                                             and ride["date"] == date)]
     
-    def getClientSubscriptions(self, object):
-        return ([request for request in self.requests if request["object"] == object],
-                [ride for ride in self.rides if ride["object"] == object])
+    def getClientSubscriptions(self, reference):
+        return ([request for request in self.requests if request["reference"] == reference],
+                [ride for ride in self.rides if ride["reference"] == reference])
 
 def main():
-    print("main")
+    print("Unimplemented server main function")
     # Registering Pyro class as a daemon in name server
     # Pyro4.Daemon.serveSimple(
     #         {

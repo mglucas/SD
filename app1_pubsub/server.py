fcc3af8ae95aa5322c696e3a71b6c712c0982668
Victor Feitosa Lourenco <victor.240398@hotmail.com>
2021-07-16 19:11:23 -0300

    Final server documentation

--- app1_pubsub/server.py
+++ app1_pubsub/server.py
@@ -14,27 +14,23 @@ Computer Engineering major at the Federal University of Technology - Parana.
 import os
 import time
 from pathlib import Path
+from threading import Thread
 import pickle
-from Pyro4.core import expose
 import serpent
 from ast import literal_eval
 from rich import print
 from rich.console import Console
-from threading import Thread
 import Pyro4
 import Pyro4.naming
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.exceptions import InvalidSignature
 """ ------------------------ """
 
 HOSTNAME = "127.0.0.1"
 
-""" 
-    TODO-Victor document stuff
-"""
-
+@Pyro4.behavior(instance_mode="session")
 class Server(object):
     def __init__(self):
         print ("[bold chartreuse3]Server[/bold chartreuse3]: Initilized a server instance")
@@ -51,16 +47,20 @@ class Server(object):
         pickledump(self.requests, 'requests')
         pickledump(self.current_id, 'current_id')
 
+
     @Pyro4.expose
     def addClient(self, name, contact, public_key):
         """
-        Description: .
+        Description: add new client with required atributes.
         
         Parameters:
-        - x (string): .
+        - name (string): client name (used as primary key for each client);
+        - contact (string): client email;
+        - public_key (bytes): serialized public key.
 
         Returns:
-        - None
+        - (bool): 'True' for when the client was added successfully and 'False'
+                  otherwise.
         """
         print("[bold chartreuse3]Server[/bold chartreuse3]: Adding client")
         self.clients = pickleload("clients")
@@ -84,16 +84,30 @@ class Server(object):
 
         return True
 
+
     @Pyro4.expose
     def addSubscription(self, message, signature):
         """
-        Description: .
+        Description: add new subscription (ride or request) to a client.
+                     The communication between the client and the server is
+                     done with a signature, which the server verifies to confirm
+                     the client's identity.
         
         Parameters:
-        - x (string): .
+        - message (string): encoded mesage from client. This contains a dictionary
+                            with 5 or 6 items depending on whether it's a ride or
+                            request:
+                            - name: name of the client (subscriber),
+                            - reference: remote object reference of the client, 
+                            - origin: selected origin of the drive,
+                            - destination: selected destination of the drive,
+                            - date:  selected date of the drive,
+                            - passengers (only for ride): max number of passengers;
+        - signature (bytes): signature of the message.
+        
 
         Returns:
-        - None
+        - self.current_id (int): unique attributed ID of the subscription.
         """
         self.clients = pickleload("clients")
         self.rides = pickleload("rides")
@@ -105,7 +119,8 @@ class Server(object):
         message_dict = message.decode('utf-8')
         message_dict = literal_eval(''.join(message_dict))
 
-        client = next((client for client in self.clients if client["name"] == message_dict["name"]), None)
+        client = next((client for client in self.clients 
+                       if client["name"] == message_dict["name"]), None)
         if client == None:
             print(f"Could not find client {message_dict['name']}!!!")
 
@@ -125,9 +140,11 @@ class Server(object):
                 ),
                 hashes.SHA256()
             )
-            print("\n[bold chartreuse3]Server[/bold chartreuse3]: Valid signature at the server!")
+            print("\n[bold chartreuse3]Server[/bold chartreuse3]: "
+                  "Valid signature at the server!")
         except InvalidSignature:
-            print("\n[bold chartreuse3]Server[/bold chartreuse3]: Invalid signature")
+            print("\n[bold chartreuse3]Server[/bold chartreuse3]: "
+                  "Invalid signature")
 
         if client["reference"] == None:
             client["reference"] = message_dict["reference"]
@@ -166,13 +183,14 @@ class Server(object):
 
         return self.current_id
 
+
     @Pyro4.expose
     def delSubscription(self, id):
         """
-        Description: .
+        Description: deletes already existing subscription.
         
         Parameters:
-        - x (string): .
+        - id (int): unique ID of the subscription to be deleted.
 
         Returns:
         - None
@@ -188,21 +206,28 @@ class Server(object):
             self.rides.remove(del_ride)
             pickledump(self.rides, "rides")
 
-    @Pyro4.expose
+
     def checkNotify(self, new_client, new_sub):
         """
-        Description: .
+        Description: method called everytime a new subscription is added to check
+                     if any other subscription matches the new one (an already
+                     existing ride for a new request or an already existing
+                     request for a new ride). Notifies correpondent client if
+                     necessary (via the remote object reference).
         
         Parameters:
-        - x (string): .
+        - new_client (dict): item from self.clients list cointaining name, contact,
+                             public key and reference;
+        - new_sub (dict): new subscriber, received as message from client, containing
+                          the items described in message (string) of the
+                          addSubscription method.
 
         Returns:
         - None
         """
         if len(new_sub) == 5:
-            matches = [ride for ride in self.rides if ride["origin"] == new_sub["origin"]
-                                                  and ride["destination"] == new_sub["destination"]
-                                                  and ride["date"] == new_sub["date"]]
+            matches = self.getAvailableRides(new_sub["origin"], new_sub["destination"], new_sub["date"])
+            
             if matches != []:
                 for match in matches:
                     client = next(client for client in self.clients if client["name"] == match["name"])
@@ -210,9 +235,8 @@ class Server(object):
                     client_p = Pyro4.Proxy(client["reference"])
                     client_p.notifyAvailablePassenger(new_client["name"], new_client["contact"])
         else:
-            matches = [request for request in self.requests if request["origin"] == new_sub["origin"]
-                                                           and request["destination"] == new_sub["destination"]
-                                                           and request["date"] == new_sub["date"]]
+            matches = self.getAvailableRequests(new_sub["origin"], new_sub["destination"], new_sub["date"])
+
             if matches != []:
                 for match in matches:
                     client = next(client for client in self.clients if client["name"] == match["name"])
@@ -220,32 +244,54 @@ class Server(object):
                     client_p = Pyro4.Proxy(client["reference"])
                     client_p.notifyAvailableDriver(new_client["name"], new_client["contact"])
     
-    @Pyro4.expose    
-    def getAvailableRides(self,origin,destination,date):
+    
+    def getAvailableRides(self, origin, destination, date):
         """
-        Description: .
+        Description: return available rides given certain constraints.
         
         Parameters:
-        - x (string): .
+        - origin (string): selected origin of the drive,
+        - destination (string): selected destination of the drive,
+        - date (string): selected date of the drive.
 
         Returns:
-        - None
+        - (dict): item from self.rides list cointaining subscription ID, client
+                  name, drive origin, destination and date and number of passangers.
         """
-        self.rides = pickleload("rides")
-        return [ride for ride in self.rides if (ride["origin"] == origin 
-                                            and ride["destination"] == destination 
-                                            and ride["date"] == date)]
+        return [ride for ride in self.rides
+                    if (ride["origin"] == origin 
+                    and ride["destination"] == destination 
+                    and ride["date"] == date)]
     
+    def getAvailableRequests(self, origin, destination, date):
+        """
+        Description: return available requests given certain constraints.
+        
+        Parameters:
+        - origin (string): selected origin of the drive,
+        - destination (string): selected destination of the drive,
+        - date (string): selected date of the drive.
+
+        Returns:
+        - (dict): item from self.requests list cointaining subscription ID, client
+                  name, drive origin, destination and date.
+        """
+        return [request for request in self.requests
+                    if (request["origin"] == origin 
+                    and request["destination"] == destination
+                    and request["date"] == date)]
+
     @Pyro4.expose
     def getClientSubscriptions(self, name):
         """
-        Description: .
+        Description: return subscriptions for a given client.
         
         Parameters:
-        - name (string): .
+        - name (string): name (unique key) of the client.
 
         Returns:
-        - None
+        - (tuple): a tuple contaning two lists: the first with the requests
+                   and the second with the rides of the given user. 
         """
         self.rides = pickleload("rides")
         self.requests = pickleload("requests")
@@ -290,6 +336,15 @@ def pickleload(name):
     return loaded_data
 
 def startNSThread():
+    """
+    Description: function responsible for initiating the name server as a thread.
+    
+    Parameters:
+    - None
+
+    Returns:
+    - None
+    """
     try: 
         print("[bold yellow]NameServer[/bold yellow]: Starting ")
         thread = Thread(target=Pyro4.naming.startNSloop,args=(HOSTNAME,))
@@ -302,6 +357,17 @@ def startNSThread():
         exit
 
 def cleanUpFiles():
+    """
+    Description: function responsible for deleting the pickles at the end of
+                 the execution of the server (thus not keeping the data on
+                 following executions).
+    
+    Parameters:
+    - None
+
+    Returns:
+    - None
+    """
     if Path("clients.pickle").is_file():
         os.remove("clients.pickle")
     if Path("current_id.pickle").is_file():
@@ -310,6 +376,7 @@ def cleanUpFiles():
         os.remove("requests.pickle")
     if Path("rides.pickle").is_file():
         os.remove("rides.pickle")
+
 
 def main():
     

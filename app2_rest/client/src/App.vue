61d941d533690b89fd180e842202ee35ee2651af
mglucas <luc0797@gmail.com>
2021-08-07 21:35:20 -0300

    client side - Basic vue components and login feat.

---
+++ app2_rest/client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1> Ride Together! </h1>
+  <LoginMenu/>
+  
+</template>
+
+<script>
+import LoginMenu from './components/LoginMenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+  background: darkgray;
+}
+</style>
